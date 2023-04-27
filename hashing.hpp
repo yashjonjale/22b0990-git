32bfd40531c7283730f94e9e8f634da7b184de3c
yashjonjale <yjonjale@gmail.com>
2023-04-27 23:43:20 +0530

    Add hashing header and string read

---
+++ hashing.hpp
@@ -0,0 +1,12 @@
+#ifndef _HASHING
+#define _HASHING
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int hash_string(string s);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include "hashing.hpp"
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    cout << hash_string(argv[1]) << endl;
+}
